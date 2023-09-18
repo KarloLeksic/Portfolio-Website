9bf7ba4fa65ef9311eeb7b33349310c8e0ec81cb
KarloLeksic <lekxka@gmail.com>
2023-09-18 17:04:26 +0200

    try to animate first section

---
+++ scripts/animations.js
@@ -0,0 +1,5 @@
+// Animacija za prvu polovicu hero sekcije
+document.querySelector('#hero-section .personal-information-container').classList.add('animate-information-container');
+
+// Animacija za pocetnu sliku
+document.querySelector('#hero-section .hero-image').classList.add('animate-hero-image');
