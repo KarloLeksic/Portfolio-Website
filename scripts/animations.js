11101330f391659145dce7586d9e70ec10973e58
KarloLeksic <lekxka@gmail.com>
2023-09-18 17:49:20 +0200

    fixed navbar scroll bug

--- scripts/animations.js
+++ scripts/animations.js
@@ -5,8 +5,7 @@ document.querySelector('#hero-section .personal-information-container').classLis
 document.querySelector('#hero-section .hero-image').classList.add('animate-hero');
 
 // Animacija na about-me sekciji tek kada se do nje dode
-window.onscroll = () => {
-
+function animateAboutSection() {
   const aboutMeSection = document.querySelector('#about-section');
 
   // Dohvacanje pozicije od vrha ekrana
@@ -24,4 +23,40 @@ window.onscroll = () => {
     aboutMeRight.classList.add('animate-about-me');
     aboutMeLeft.classList.add('animate-about-me');
   }
+}
+
+// Za animiranje navbara kak se skrola
+const sidemenu = document.querySelector('#side-menu');
+
+// Otvaranje responzivnog menija kada je na malom ekranu
+document.querySelector('.bx-menu-alt-right').addEventListener('click', () => {
+  sidemenu.style.right = '0';
+});
+
+// Zatvaranje responzivnog menija
+document.querySelector('.bx-x').addEventListener('click', () => {
+  sidemenu.style.right = '-200px';
+});
+
+// Za automatsko prebacivanje aktivnih linkova u gornjem izborniku
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header ul li a');
+
+window.onscroll = () => {
+  sections.forEach(section => {
+    const top = window.scrollY;
+    const offset = section.offsetTop - 500;
+    const height = section.offsetHeight;
+    const id = section.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+
+      document.querySelector('header ul li a[href*=' + id + ']').classList.add('active');
+    }
+  });
+
+  animateAboutSection();
 };
