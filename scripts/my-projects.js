ec4bb4c4def4516c7f201c0224adbe9284b8c580
KarloLeksic <lekxka@gmail.com>
2023-09-20 16:38:27 +0200

    added select category functionality in my work section

---
+++ scripts/my-projects.js
@@ -0,0 +1,60 @@
+// Ovo je kao neka baza podataka, od tu on vuce podatke o projektima na zadnjoj sekciji
+// Potrebno radi generiranja prema kategorijama
+const projects = [
+  {
+    imagePath: 'images/projects/led-corner-lamp.jpg',
+    name: 'LED Corner Lamp',
+    description: 'Homemade corner lamp made of wooden pieces and a 3D printed part that connects them. A much cheaper and more versatile variant.',
+    link: 'https://www.instagram.com/reel/Cp8DsFwOnT7/',
+    category: 'leds, 3d-prints'
+  },
+  {
+    imagePath: 'images/projects/led-pool-1.jpg',
+    name: 'LED Pool Table',
+    description: 'I tried to make something unique, something that I have never seen anywhere. It looks very cool.',
+    link: '',
+    category: 'leds'
+  },
+  {
+    imagePath: 'images/projects/led-room-lana.jpg',
+    name: 'LED Room',
+    description: 'I love when everything lights up, it looks so good, but you have to know how to do it. I also do construction work so that everything looks nice and professional in the end.',
+    link: 'https://www.instagram.com/reel/CZsLiBeAfFJ/',
+    category: 'leds'
+  },
+  {
+    imagePath: 'images/projects/3d-printed-tap.jpg',
+    name: '3D Printed Tap Handle',
+    description: 'I love when I can use my hobbies for something really useful. I 3D-printed a tap handle that was broken.',
+    link: 'https://www.instagram.com/reel/CrGgdxgpj2P/',
+    category: '3d-prints'
+  },
+  {
+    imagePath: 'images/projects/beer-maker-2.jpg',
+    name: 'Temperature control device',
+    description: 'I like projects like this the most because I design and do everything myself. Just the way I want it and the way I want it. The device is used for the production of beer.',
+    link: 'https://www.instagram.com/reel/CwIqncGsbQl/',
+    category: '3d-prints, electronics'
+  },
+  {
+    imagePath: 'images/projects/happy-leds-app.jpg',
+    name: 'Happy LEDs App',
+    description: 'My first IoT project from scratch. Control of led lights via mobile phone.',
+    link: 'https://github.com/KarloLeksic/HappyLEDs-IoT-project',
+    category: 'leds'
+  },
+  {
+    imagePath: 'images/projects/gif/robotic-arm.gif',
+    name: 'Remote Controlled Robotic Arm',
+    description: 'My final paper in college. At that time I didn\'t know how to draw in 3D, but everything else is my work inspired by videos from youtube.',
+    link: 'https://www.instagram.com/reel/CfGzTHGODpx/',
+    category: 'electronics, 3d-prints'
+  },
+  {
+    imagePath: 'images/projects/IoT-sensor-printed-box.jpg',
+    name: 'Some Smart Home Prints',
+    description: 'Unfortunately, I don\'t have my own house or apartment, so this is improvised, but the 3d printed cases for the sensors look very cool.',
+    link: 'https://www.instagram.com/reel/CsMLpKAsanr/',
+    category: '3d-prints, electronics'
+  }
+];
--- scripts/my-work-generator.js
+++ scripts/my-work-generator.js
@@ -1,14 +1,62 @@
 const buttons = document.querySelectorAll('#my-work button');
 
+// Listener na svaki gumb kad se klikne da se generiraju projekti samo za tu kategoriju
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
+    // Micanje active klase sa svih
     buttons.forEach(b => {
       b.classList.remove('active');
     });
 
+    // Dodavanje active kolase samo gumbu koji je kliknut
     btn.classList.add('active');
 
-    // Generiranje sadržaja ovisno o pritisnutim butonima
+    // Generiranje sadržaja ovisno o pritisnutom gumbu
+    const selectedCategory = btn.getAttribute('id');
+    generateProjects(selectedCategory);
   });
 });
 
+// Po defaultu generiraj sve
+generateProjects('all');
+
+// Generiranje projekata na stranici u ovisnosti o kategorija
+function generateProjects(selectedCategory) {
+  // Dohvati element u koji se ubacuju projekti
+  const projectContainerElement = document.querySelector('.work-list');
+
+  // Brisanje onih koji su tamo
+  projectContainerElement.innerHTML = '';
+
+  // Idemo kroz sve projecte
+  projects.forEach(project => {
+    // Ako je odabrano na sve onda ih prikazujemo sve
+    let html = '';
+    if (selectedCategory === 'all') {
+      html = generateProjectHtml(project.imagePath, project.name, project.description, project.link, project.category);
+    } else {
+      // Ako je to ta kategorija koju zelimo, generiraj html i popunjavaj redom grid
+      if (project.category.includes(selectedCategory)) {
+        html = generateProjectHtml(project.imagePath, project.name, project.description, project.link, project.category);
+      }
+    }
+
+    projectContainerElement.innerHTML += html;
+  });
+}
+
+// Funkcija koja generira html za jedan projekt
+function generateProjectHtml(imagePath, name, description, link, category) {
+  return `
+    <div class="card" data-category="${category}">
+    <img src="${imagePath}">
+
+      <div class="layer">
+        <h3>${name}</h3>
+        <p>${description}</p>
+        <a href="${link}" target="_blank">
+        <i class='bx bx-link-external'></i></a>
+      </div>
+    </div>
+  `;
+}
