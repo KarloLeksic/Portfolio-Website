3faf95198ac044667c44b0c50a7584ecc921eab7
KarloLeksic <lekxka@gmail.com>
2023-09-20 18:19:14 +0200

    manual responsive because it is better when projects are generated with equal spacing if they are less than the width of the screen

--- scripts/animations.js
+++ scripts/animations.js
@@ -56,7 +56,7 @@ function animateProjectsSection(screenPosition) {
   const projectSectionPosition = projectsSection.getBoundingClientRect().top;
   const titleAndButtonsContainer = document.querySelector('.title-btns-container');
 
-  if (projectSectionPosition < screenPosition / 1.4) {
+  if (projectSectionPosition < screenPosition / 1.3) {
     titleAndButtonsContainer.classList.add('animate-work-title');
 
     // Po defaultu generiraj sve
